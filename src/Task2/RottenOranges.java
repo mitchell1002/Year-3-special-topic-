5a15e4077b2de30bb9606510f1aa571eed04cf3a
mitchell1002 <124736202+mitchell1002@users.noreply.github.com>
2024-08-29 09:46:14 +1200

    this is my Task 2 version 1.2

--- src/Task2/RottenOranges.java
+++ src/Task2/RottenOranges.java
@@ -1,66 +1,123 @@
 package Task2;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
-public class RottenOranges {
+class Queue {
+    Node head, tail;
+    int size;
 
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public Queue() {
+        this.tail = null;
+        this.head = null;
+        this.size = 0; // Initialize size to 0
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void enqueue(int item) {
+        Node node = new Node(item);
+        if (isEmpty()) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+        size++; // Increment size when enqueueing
+    }
+
+    public int dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return Integer.MIN_VALUE; // Return a sentinel value if the queue is empty
+        }
+        int item = head.data;
+        if (head != tail) {
+            head = head.next;
+        } else {
+            head = null; // If there was only one node, set head and tail to null
+            tail = null;
+        }
+        size--; // Decrement size when dequeueing
+        return item;
+    }
+
+    public int getSize() {
+        return size; // Return the current size of the queue
+    }
+}
+
+class RottenOranges {
     public static void userInput(Scanner scanner, int[][] grid) {
-        System.out.println("Please enter  the values of the oranges ");
+        System.out.println("Please enter the values of the oranges: ");
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                grid[i][j] = scanner.nextInt();
+                grid[i][j] = scanner.nextInt(); // Read the integers from the user and fills in the grid
             }
-        }//on this method 1 am getting user input for the rows and columns they would like
-        //to place their oranges in, i and j representing the grid height and width
+        } // On this method, I am getting user input for the rows and columns they would like
+        // to place their oranges in, i and j representing the grid height and width
         // using scanner is reading the integers from the user and fills in the grid
     }
 
-    private static int ReadGrid(int[][] grid) {
-        Queue<int[]> queue = new LinkedList<>();
-        int fresh = 0;// this keeps track of the oranges the user puts
-        // l am using queue because it will store the position of each orange and if the fresh one gets rotten see if the next orange gets rotten
+    private static int readGrid(int[][] grid) {
+        Queue queue = new Queue(); // Create an instance of the Queue
+        int fresh = 0; // This keeps track of the oranges the user puts
+        // I am using queue because it will store the position of each orange and if the fresh one gets rotten see if the next orange gets rotten
 
-
-        for (int i = 0; i < grid.length; i++) { // this loops around the grid to detect the fresh ones
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 2) queue.offer(new int[]{i, j});    // Initialize the queue with rotten oranges and count fresh ones
+        // Enqueue rotten oranges and count fresh oranges
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) { // Enqueue
+                if (grid[i][j] == 2) {
+                    queue.enqueue(i * grid[0].length + j); // Encode coordinates into a single integer
+                }
                 if (grid[i][j] == 1) fresh++;
             }
         }
 
+        int time = 0;
 
-
-    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};// this for the four possible directions
-
-    int time = 0;
+        // Process rotten oranges until none are left or all are rotten
         while (!queue.isEmpty() && fresh > 0) {
-        int size = queue.size();
-        //time++;// keeping the track of the secons or minutes that have passed by
-        //System.out.println("Time: " + time);
-            // this is the while loop that keeps track of how many oranges have rot and how long did it take ti rot them
+            int size = queue.getSize(); // Get the current size of the queue
+            // Time++; // Keeping track of the seconds or minutes that have passed by
+            // System.out.println("Time: " + time);
+            // This is the while loop that keeps track of how many oranges have rotted and how long did it take to rot them
             // until they are no more to rot
-        showFreshOranges(grid);
 
-        while (size-- > 0) {// runs how many oranges are rotten  size is counting down how many rotten oranges in the time span
-            int[] curr = queue.poll();
-            for (int[] d : dirs) {
-                int x = curr[0] + d[0], y = curr[1] + d[1];// this is for directions adding the next cells
-                if (x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[x][y] == 1) {// this checks if  the grid is not out of dimensions
-                    grid[x][y] = 2;//fresh orange gets rotten
-                    queue.offer(new int[]{x, y}); // this adds to the queue structure so that they can begin with next cells
-                    fresh--;
-                    System.out.println("Orange at (" + x + ", " + y + ") got rotten.");
+            while (size-- > 0) { // Runs how many oranges are rotten. Size is counting down how many rotten oranges in the time span
+                int curr = queue.dequeue(); // Get the current rotten orange
+                int x = curr / grid[0].length; // Decode coordinates
+                int y = curr % grid[0].length;
+
+                int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // Directions
+                for (int[] d : dirs) {
+                    int newX = x + d[0], newY = y + d[1];
+                    if (newX >= 0 && newY >= 0 && newX < grid.length && newY < grid[0].length && grid[newX][newY] == 1) {
+                        grid[newX][newY] = 2; // Mark the orange as rotten
+                        queue.enqueue(newX * grid[0].length + newY); // Enqueue the new rotten orange
+                        fresh--;
+                        System.out.println("Orange at (" + newX + ", " + newY + ") got rotten.");
+                    }
                 }
             }
+            time++; // Increment time after processing all rotten oranges in this round
         }
-    }
 
-        return fresh == 0 ? time : -1;
-        // this is checking if the fresh oranges are rotten or not with ?starts another else statement
-        //and the total time it took to rot oranges
-}
+        return fresh == 0 ? time : -1; // This is checking if the fresh oranges are rotten or not. With ? starts another else statement
+        // and the total time it took to rot oranges
+    }
 
     private static void showFreshOranges(int[][] grid) {
         System.out.println("Fresh oranges remaining:");
@@ -70,22 +127,17 @@ public class RottenOranges {
                     System.out.println("(" + i + ", " + j + ")");
                 }
             }
-        }// this loops checks if we still have isolated 1 which is fresh oranges and then prints out
+        } // This loops checks if we still have isolated 1 which is fresh oranges and then prints out
     }
-
-
-
 
     private static void displayResults(int time, int[][] grid) {
         if (time != -1) {
-            int seconds = time % 60;
-            System.out.println("All oranges have rotted in : " + seconds+ "seconds ");
+            System.out.println("All oranges have rotted in: " + time + " seconds.");
         } else {
             System.out.println("Fresh oranges remaining at:");
             System.out.println("Not all oranges can rot.");
-
             showFreshOranges(grid);
-        }// this one l know
+        }
     }
 
     public static void main(String[] args) {
@@ -98,13 +150,12 @@ public class RottenOranges {
 
         int[][] grid = new int[rows][cols];
 
-        userInput(scanner, grid);// calling the method
+        userInput(scanner, grid);
 
-        int time = ReadGrid(grid);//calling the function
+        int time = readGrid(grid);
 
-        displayResults(time, grid);//calling method
+        displayResults(time, grid);
 
-        scanner.close();// this avoids the resource leaks
+        scanner.close();
     }
-
 }
